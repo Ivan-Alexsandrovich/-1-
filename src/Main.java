e99e4894c3819db56c5d52e2ef80f60c26798011
user <https://github.com/@Ivan-Alexsandrovich/Holloworld.git>
2024-05-03 12:17:10 +0800

    initin2

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,50 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ege = 19;
+        int ege = 21;
         if (ege >= 18) {
             System.out.println("совершенолетие");
         } else {
-            System.out.println("то недостиг совершенолетия");
+            System.out.println("он не достиг совершеннолетия, нужно немного подождать».");
         }
         System.out.println();
 
-        int temp = 6;
+        int temp = 4;
         if (temp <= 5) {
-            System.out.println("на улице холодно ,нужно надеть шапку");
+            System.out.println("на улице холодно , нужно надеть шапку");
         } else {
             System.out.println("сегодня тепло , можно идти без шапки");
         }
         System.out.println();
 
-        int speed = 60;
+        int speed = 50;
         if (speed > 60) {
-            System.out.println("скорость превышена,придется заплотить штраф");
+            System.out.println("скорость превышена, если она больше 60 км/ч;");
         } else {
-            System.out.println("привышение скорости нет,можно ехать");
+            System.out.println("превышения скорости нет, если она меньше 60 км/ч.");
         }
         System.out.println();
 
-        int personAge = 32;
+        int personAge = 15;
         if (personAge >= 2 && personAge <= 6) {
-            System.out.println("ему нужно в  дет. сад");
+            System.out.println("ему нужно ходить в детский сад.");
         }
         if (personAge >= 7 && personAge <= 17) {
-            System.out.println(" ему нужно в школу");
+            System.out.println(" ему нужно ходить в школу");
         }
         if (personAge >= 18 && personAge <= 24) {
-            System.out.println("ему нужно в университет");
+            System.out.println("его место в университете.");
         }
         if (personAge > 24) {
-            System.out.println("нужно идти на работу");
+            System.out.println("пора ходить на работу");
         }
         System.out.println();
 
-        int child = 18;
-        if (child > 5) {
+        int child = 4;
+        if (child < 5) {
             System.out.println("нельзя кататься");
         }
-        if (child < 14) {
+        if (child > 5 && child < 14) {
             System.out.println("можно,только с взрослым");
         }
         if (child > 14) {
@@ -53,12 +53,12 @@ public class Main {
         System.out.println();
 
         int total = 102;
-        int seadTotal = 60;
-        int standing = 25;
-        if (standing > total) {
+        int sead = 60;
+        int place = 103;
+        if (place > total) {
             System.out.println("мест нет");
         } else {
-            if (standing > seadTotal) {
+            if (place > sead) {
                 System.out.println("стоим");
             } else {
                 System.out.println("сидим");
